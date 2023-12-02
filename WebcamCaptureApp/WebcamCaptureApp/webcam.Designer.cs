3f689c596adc4c2f6b69f77df4589e261c08b768
nadiah2323 <114987104+nadiah2323@users.noreply.github.com>
2023-12-02 22:38:32 +0700

    Add files via upload

---
+++ WebcamCaptureApp/WebcamCaptureApp/webcam.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WebcamCaptureApp
+{
+    partial class webcam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webcamBox = new System.Windows.Forms.ComboBox();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.captbtn = new System.Windows.Forms.Button();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webcamBox
+            // 
+            this.webcamBox.FormattingEnabled = true;
+            this.webcamBox.Location = new System.Drawing.Point(12, 15);
+            this.webcamBox.Name = "webcamBox";
+            this.webcamBox.Size = new System.Drawing.Size(121, 21);
+            this.webcamBox.TabIndex = 0;
+            // 
+            // startbtn
+            // 
+            this.startbtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.startbtn.Location = new System.Drawing.Point(159, 15);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(96, 21);
+            this.startbtn.TabIndex = 1;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = false;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // captbtn
+            // 
+            this.captbtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.captbtn.Location = new System.Drawing.Point(261, 15);
+            this.captbtn.Name = "captbtn";
+            this.captbtn.Size = new System.Drawing.Size(96, 21);
+            this.captbtn.TabIndex = 2;
+            this.captbtn.Text = "Capture";
+            this.captbtn.UseVisualStyleBackColor = false;
+            this.captbtn.Click += new System.EventHandler(this.captbtn_Click);
+            // 
+            // savebtn
+            // 
+            this.savebtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.savebtn.Location = new System.Drawing.Point(363, 15);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(96, 21);
+            this.savebtn.TabIndex = 3;
+            this.savebtn.Text = "Save Image";
+            this.savebtn.UseVisualStyleBackColor = false;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.exitbtn.Location = new System.Drawing.Point(692, 15);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(96, 21);
+            this.exitbtn.TabIndex = 4;
+            this.exitbtn.Text = "Exit";
+            this.exitbtn.UseVisualStyleBackColor = false;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 59);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 380);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 374);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(391, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(382, 374);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // webcam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.captbtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.webcamBox);
+            this.Name = "webcam";
+            this.Text = "Webcam";
+            this.Load += new System.EventHandler(this.webcam_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox webcamBox;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Button captbtn;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WebcamCaptureApp/WebcamCaptureApp/webcam.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+
+namespace WebcamCaptureApp
+{
+    public partial class webcam : Form
+    {
+        private FilterInfoCollection captureDevice;
+        private VideoCaptureDevice videoSource;
+        private ImageFormat imageFormat;
+
+        public webcam()
+        {
+            InitializeComponent();
+        }
+
+        private void webcam_Load(object sender, EventArgs e)
+        {
+            captureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo deviceList in captureDevice)
+            {
+                webcamBox.Items.Add(deviceList.Name);
+            }
+            webcamBox.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            videoSource = new VideoCaptureDevice(captureDevice[webcamBox.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void captbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save Image As";
+            saveFileDialog.Filter = "Image files (*.jpg, *.png) | *.jpg, *.png";
+            ImageFormat format = ImageFormat.Jpeg;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(saveFileDialog.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        imageFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        imageFormat = ImageFormat.Png;
+                        break;
+                }
+                pictureBox2.Image.Save(saveFileDialog.FileName, imageFormat);
+            }
+        }
+
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+                pictureBox2.Image = null;
+                pictureBox2.Invalidate();
+            }
+            Application.Exit(null);
+        }
+    }
+}
